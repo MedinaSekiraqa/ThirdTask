e3a6777c1bc25bff68ad66d0d4fec468d739b3b8
MedinaSekiraqa <102260547+MedinaSekiraqa@users.noreply.github.com>
2023-12-28 15:20:21 +0100

    first commit

---
+++ src/components/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Popup(){
+    return (
+        <div className='popup'>
+            <div className='popup-inner'>
+                <button className='close-btn'>close</button>
+
+            </div>
+
+        </div>
+    )
+}
+export default Popup
